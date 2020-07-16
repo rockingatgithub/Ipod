a08e0660428670950bf697a914e589bff3fe60c4
rockingatgithub <contactwithsid@gmail.com>
2020-07-16 20:47:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ZingTouch from 'zingtouch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(){
+    super();
+
+  }
+  handleClick(){
+    console.log('clicked');
+    // let ParentTouchArea = document.getElementById('mid');
+    let touchArea = document.getElementById('mid');
+    let myRegion = new ZingTouch.Region(touchArea);
+    myRegion.bind(touchArea, 'rotate', function(e){
+      console.log(e.detail.angle);
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+      <div id="outer">
+        <div id="mid">
+        </div>
+        <div className="menu-button">MENU</div>
+          <img src="https://image.flaticon.com/icons/svg/39/39712.svg" id="prev" alt="forward" height="16px" width="16px" />
+          <div id="inner" onMouseDown={this.handleClick} draggable="false">
+          </div>
+          <img src="https://image.flaticon.com/icons/svg/724/724939.svg" id="forward" alt="previous" height="16px" width="16px" />
+          <img src="https://image.flaticon.com/icons/svg/2404/2404317.svg" id="play-pause" alt="play/pause" height="24px" width="24px" />
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
